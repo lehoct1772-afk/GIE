3af32d1aca95d8f0a47873df1c493c5c3ebf1fca
Lawrence Herber II <leh.oct1772@gmail.com>
2026-07-28 23:27:20 -0500

    Create geometry package

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Geometric Intelligence Engine (GIE)
+
+Core geometry package.
+"""
+
+from .geometry import Point
+from .line import Line
+from .circle import Circle
+from .rectangle import Rectangle
