01b13a23f2468bfab83b49b2cd182087b20e816d
Lawrence Herber II <leh.oct1772@gmail.com>
2026-07-28 22:49:01 -0500

    Add main application entry point

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Geometric Intelligence Engine (GIE)
+Main Entry Point
+"""
+
+def main():
+    print("Geometric Intelligence Engine")
+    print("Version 0.1.0")
+
+
+if __name__ == "__main__":
+    main()
