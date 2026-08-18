1fa46a5644a163e3ccdb3a1baf955a7abffa7551
Lawrence Herber II <leh.oct1772@gmail.com>
2026-08-18 05:54:03 -0500

    Repair Milk Hill PDF public assets and manifest

---
+++ src/milkHillPdfs.ts
@@ -0,0 +1,77 @@
+﻿export interface MilkHillPdf {
+  title: string;
+  file: string;
+}
+
+export const milkHillPdfs: MilkHillPdf[] = [
+  {
+    title: '01_Milk_Hill_2001_Measurement_Basis_787FT_COMPLETE',
+    file: '/research/milk-hill/01_Milk_Hill_2001_Measurement_Basis_787FT_COMPLETE.pdf',
+  },
+  {
+    title: '02_Milk_Hill_2001_409_COMPLETE_Physical_Reconstruction',
+    file: '/research/milk-hill/02_Milk_Hill_2001_409_COMPLETE_Physical_Reconstruction.pdf',
+  },
+  {
+    title: '03_Milk_Hill_2001_Fibonacci_Golden_Spiral_Full_Test',
+    file: '/research/milk-hill/03_Milk_Hill_2001_Fibonacci_Golden_Spiral_Full_Test.pdf',
+  },
+  {
+    title: '04_Milk_Hill_2001_409_Binary_and_FiveBit_Full_Record',
+    file: '/research/milk-hill/04_Milk_Hill_2001_409_Binary_and_FiveBit_Full_Record.pdf',
+  },
+  {
+    title: '05_Milk_Hill_2001_Letter_Permutation_Test',
+    file: '/research/milk-hill/05_Milk_Hill_2001_Letter_Permutation_Test.pdf',
+  },
+  {
+    title: '06_Milk_Hill_2001_FINAL_MATHEMATICAL_CONCLUSION',
+    file: '/research/milk-hill/06_Milk_Hill_2001_FINAL_MATHEMATICAL_CONCLUSION.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_2001_Mathematical_Supporting_Record_PREVIEW',
+    file: '/research/milk-hill/GIE_Milk_Hill_2001_Mathematical_Supporting_Record_PREVIEW.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_2001_Mathematical_Supporting_Record_PREVIEW',
+    file: '/research/milk-hill/GIE_Milk_Hill_2001_Mathematical_Supporting_Record_PREVIEW-2.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_Blueprint_001_Analysis_Record_PREVIEW',
+    file: '/research/milk-hill/GIE_Milk_Hill_Blueprint_001_Analysis_Record_PREVIEW.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_Blueprint_001_Analysis_Record_PREVIEW',
+    file: '/research/milk-hill/GIE_Milk_Hill_Blueprint_001_Analysis_Record_PREVIEW-2.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_20260816',
+    file: '/research/milk-hill/GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_20260816.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_CONTINUED_20260816',
+    file: '/research/milk-hill/GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_CONTINUED_20260816.pdf',
+  },
+  {
+    title: 'GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_PHASE_RING_20260816',
+    file: '/research/milk-hill/GIE_Milk_Hill_Spiral_Arm_Boundary_Analysis_PHASE_RING_20260816.pdf',
+  },
+  {
+    title: 'Milk_Hill_2001_actual_math_binary_analysis',
+    file: '/research/milk-hill/Milk_Hill_2001_actual_math_binary_analysis.pdf',
+  },
+  {
+    title: 'Milk_Hill_2001_actual_math_binary_analysis',
+    file: '/research/milk-hill/Milk_Hill_2001_actual_math_binary_analysis-2.pdf',
+  },
+  {
+    title: 'Milk_Hill_2001_MATHEMATICAL_RESULT_FINAL_WITH_VISUALS',
+    file: '/research/milk-hill/Milk_Hill_2001_MATHEMATICAL_RESULT_FINAL_WITH_VISUALS.pdf',
+  },
+  {
+    title: 'Milk_Hill_2001_MATHEMATICAL_RESULT_FINAL_WITH_VISUALS',
+    file: '/research/milk-hill/Milk_Hill_2001_MATHEMATICAL_RESULT_FINAL_WITH_VISUALS-2.pdf',
+  },
+];
+
+export default milkHillPdfs;
